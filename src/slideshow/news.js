600680864251ccefe716512ea3bb517aaad389bc
slowtacocar <13561297+slowtacocar@users.noreply.github.com>
2020-07-29 11:00:23 -0400

    Remove jQuery from news

--- src/slideshow/news.js
+++ src/slideshow/news.js
@@ -1,5 +1,3 @@
-import jQuery from "jquery";
-
 const NEWS_DELAY = 1000;
 const NEWS_SPEED = 100;
 const TRANSITION_DELAY = 500;
@@ -8,43 +6,41 @@ const RSS_API_URL = "https://api.rss2json.com/v1/api.json?rss_url=";
 class News {
   constructor() {
     this.isRunning = false;
-    this.cardNews = jQuery("#cardNews");
-    this.cardNews.on("transitionend", this.loop);
+    this.cardNews = document.getElementById("cardNews");
+    this.cardNews.addEventListener("transitionend", this.loop);
   }
 
   loop = () => {
     this.isRunning = true;
-    this.cardNews.css({
-      "transform": "translate(100vw)",
-      "transition": "none"
-    });
+    this.cardNews.style.transform = "translate(100vw)";
+    this.cardNews.style.transition = "none";
+    this.news = document.getElementById("news");
     Promise.all(this.links.map(this.getSource)).then(this.setText);
     window.setTimeout(this.setTransition, TRANSITION_DELAY);
     window.setTimeout(this.setTransform, NEWS_DELAY);
   };
 
-  getSource = (link) => {
-    const params = { "url": RSS_API_URL + link };
+  getSource = (link) => fetch(RSS_API_URL + link).then(this.getJSON);
 
-    return jQuery.ajax(params).then(this.getItems);
-  };
+  getJSON = (response) => response.json().then(this.getItems);
 
   getItems = (data) => data.items.map(this.getTitle).join(" \u25cf ");
 
   getTitle = (item) => item.title;
 
   setText = (texts) => {
-    jQuery("#news").text(texts.join(""));
+    this.news.textContent = texts.join("");
   };
 
   setTransition = () => {
-    const width = this.cardNews.width() + jQuery(window).width();
+    const width = getComputedStyle(this.cardNews).width.replace("px", "") +
+      window.innerWidth;
 
-    this.cardNews.css("transition", `transform ${width / NEWS_SPEED}s linear`);
+    this.cardNews.style.transition = `transform ${width / NEWS_SPEED}s linear`;
   };
 
   setTransform = () => {
-    this.cardNews.css("transform", "translate(-100%)");
+    this.cardNews.style.transform = "translate(-100%)";
   };
 
   setLinks = (links) => {
