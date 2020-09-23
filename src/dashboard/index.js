62062e11dc271830d76070582ce661fa80fed748
slowtacocar <13561297+slowtacocar@users.noreply.github.com>
2020-09-22 20:41:24 -0400

    Support v1-style link format

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.module.css";
 import Dashboard from "./Dashboard";
 
+// TODO: Remove this once all users have updated their slideshow link
+if (new URLSearchParams(location.search).get("redir") === "slideshow.html") {
+  location.replace("slideshow.html");
+}
+
 ReactDOM.render(<Dashboard />, document.getElementById("root"));
